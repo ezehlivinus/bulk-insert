3101c3896e27aca95d2d58e51382d16a92573c23
Ezeh Livinus <ezehlivinusezeh@gmail.com>
2021-02-16 23:37:03 +0100

    Fix: hello

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const start = require('./start/kernel');
 let knex = Knex(database.development);
 
 if (process.env.NODE_ENV === 'production') {
-  knex = Knex(process.env.DATABASE_URL);
+  knex = Knex(process.env._DATABASE_URL);
 }
 
 Model.knex(knex);
